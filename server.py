ab0104d9f7bff765a76074f04e3b3ed15209b35f
AangGuns <anggun.1998@students.amikom.ac.id>
2021-05-05 09:49:07 +0700

    final presentasi

--- server.py
+++ server.py
@@ -64,4 +64,4 @@ if __name__ == '__main__':
 	#default
 	app.run(debug=True) 
     # app.run(host='localhost', port=5000)
-	# serve(app, host="0.0.0.0", port=8080)
+	# serve(app, host="0.0.0.0", port=8080) tes
